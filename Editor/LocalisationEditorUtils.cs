9f5c1be0fa896d21732a7002eb0db125d77abcc0
Jamie Healey <jamie.healey@xrgames.io>
2019-03-18 14:32:10 +0000

    Optimize imports

--- Editor/LocalisationEditorUtils.cs
+++ Editor/LocalisationEditorUtils.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using DUCK.Localisation.LocalisedObjects;
-using UnityEditor;
+﻿using UnityEditor;
 
 namespace DUCK.Localisation.Editor
 {
